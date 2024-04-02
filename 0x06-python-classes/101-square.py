be496c55939d8abd0865826ace946ca145a3d529
AhmdWahba <ahmd.wahba@outlook.comgit config --global user.name AhmdWahba>
2024-04-03 01:57:13 +0200

    updated

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
-'''Define a Square class.'''
-
 
 class Square:
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
 
     def my_print(self):
-        '''Print the square with the number character.'''
         if self.__size == 0:
             print("")
             return
@@ -19,7 +43,6 @@ class Square:
             print("#" * self.__size)
 
     def __str__(self):
-        '''Define the print() representation of a Square.'''
         string_representation = ""
         if self.__size != 0:
             string_representation += "\n" * self.__position[1]
@@ -29,4 +52,3 @@ class Square:
             if _ != self.__size - 1:
                 string_representation += "\n"
         return string_representation
-      
