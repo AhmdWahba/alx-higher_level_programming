783908059e7b77ef65a4a71ae87da6aa9adaf8d6
AhmdWahba <ahmd.wahba@outlook.comgit config --global user.name AhmdWahba>
2024-04-27 03:03:02 +0300

    updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,102 +1,109 @@
 #!/usr/bin/python3
-"""Rectangle inherit from Base"""
+"""This module contains a rectangle class"""
+
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle an instance of Base"""
+    """Represents a rectangle """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """The constructor method"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        """Initializes attributes of the object"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
+    # List of getter functions
     @property
     def width(self):
-        """get the width"""
-        return (self.__width)
-
-    @width.setter
-    def width(self, value):
-        """set the width"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
+        """Gets the value for width"""
+        return self.__width
 
     @property
     def height(self):
-        """get the height"""
-        return (self.__height)
-
-    @height.setter
-    def height(self, value):
-        """set he height"""
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
+        """Gets the value for height"""
+        return self.__height
 
     @property
     def x(self):
-        """get x"""
-        return (self.__x)
-
-    @x.setter
-    def x(self, value):
-        """set x"""
-        if type(value) != int:
-           raise TypeError("x must be an integer")
-        if value < 0:
-           raise ValueError("x must be >= 0")
-        self.__x = value
+        """Gets the value for x"""
+        return self.__x
 
     @property
     def y(self):
-        """get y"""
-        return (self.__y)
+        """Gets the value for y"""
+        return self.__y
+
+    # List of setter functions
+    @width.setter
+    def width(self, value):
+        """Sets the value for width"""
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets the value for height"""
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Sets the value for x"""
+        if (type(value) is not int):
+            raise TypeError("x must be an integer")
+
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
 
     @y.setter
     def y(self, value):
-        """set y"""
-        if type(value) != int:
+        """Sets the value for y"""
+        if (type(value) is not int):
             raise TypeError("y must be an integer")
+
         if value < 0:
             raise ValueError("y must be >= 0")
+
         self.__y = value
 
     def area(self):
-        """calculate the area of the rectangle"""
-        return (self.__width * self.__height)
+        """Defines the area of the rectangle"""
+        return (self.__height * self.__width)
 
     def display(self):
-        """dispaly to the stdout"""
-        if self.width == 0 or self.height == 0:
+        """Displays the rectangle using # """
+        for y in range(self.y):
             print("")
-            return
-
-        [print("") for y in range(self.y)]
-        for h in range(self.height):
-            [print(" ", end="") for x in range(self.x)]
-            [print("#", end="") for w in range(self.width)]
-            print("")
+        for row in range(self.__height):
+            for x in range(self.x):
+                print(" ", end="")
+            for column in range(self.__width):
+                print("#", end="")
+            print()
 
     def __str__(self):
-        """print statement"""
-        value = "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                        self.__x,
-                                                        self.__y,
-                                                        self.__width,
-                                                        self.__height)
-        return (value)
+        """Defines a format for the string representation of the class"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+{self.__width}/{self.__height}"
 
     def update(self, *args, **kwargs):
-        """update the argument"""
+        """Assigns an argument to each attribute"""
+
         if args and len(args) != 0:
             a = 0
             for arg in args:
@@ -114,6 +121,7 @@ class Rectangle(Base):
                 elif a == 4:
                     self.y = arg
                 a += 1
+
         elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "id":
@@ -131,10 +139,10 @@ class Rectangle(Base):
                     self.y = v
 
     def to_dictionary(self):
-        """return the json in the dictionary form"""
-        return {'id': self.id,
-                 'width': self.width,
-                 'height': self.height,
-                 'x': self.x,
-                 'y': self.y}
+        """Returns the dictionary representation of a Rectangle"""
 
+        obj_dictionary = {'id': self.id, 'width': self.__width,
+                          'height': self.__height, 'x': self.__x,
+                          'y': self.__y}
+
+        return obj_dictionary
